9377ff5825effb1a196d9a5f1df2b283648db318
gabrieloliveirasouza21 <gabrielosouza21@gmail.com>
2023-10-17 17:59:59 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program {
+    private static void Main(string[] args) {
+        void ReverterString(string word) {
+
+            char[] Cword = word.ToCharArray();
+            string resultante = "";
+            for (int i = Cword.Length - 1; i >= 0; i--) {
+                resultante += Cword[i].ToString();
+            }
+
+            Console.WriteLine(resultante);
+
+        }
+
+        ReverterString("teste de inversão de string");
+    }
+}
